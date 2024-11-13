3c13c66b5c1ea5693c91fdf1974dfa3e4d3fa61d
Insilises <penguin21492@gmail.com>
2024-11-13 09:25:52 -0600

    Bugged - Top half of robot rotates wrong direction

---
+++ DrBrownCode/2D Rotations/matrices.js
@@ -0,0 +1,24 @@
+function MatrixMultiply(u, v)
+{
+  var w = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
+  for (var i=0; i<3; i++)
+  {
+    for (var j=0; j<3; j++)
+    {
+      w[i][j] = (u[i][0] * v[0][j]) + (u[i][1] * v[1][j]) + (u[i][2] * v[2][j]);
+    }
+  }
+  return w;
+}
+
+function VectorMultiply(m, p)
+{
+  var q = [];
+  for (var i=0; i<3; i++)
+  {
+    q[i] = (m[i][0] * p[0] + m[i][1] * p[1] + m[i][2] * p[2]);
+  }
+  return q;
+}
+
+
